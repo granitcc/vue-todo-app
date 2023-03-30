7f74eb7441bd9b556f20bc8f82f7a8c2541666f8
Graniti <granitcakaj@outlook.com>
2023-03-30 05:09:08 +0200

    added a description text

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+      Welcome to my Todo application built with Vue.js 3! The purpose of this
+      app is to help users manage their tasks and stay organized in a simple and
+      intuitive way. Whether you need to keep track of your work assignments,
+      personal projects, or daily errands, this app has got you covered.
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<style lang="scss" scoped>
+.about {
+  font-size: 29px;
+  width: 100%;
+  display: flex;
+  padding: 20rem;
+  align-items: center;
+  justify-content: center; 
+  @media screen and (max-width: 1024px) {
+    padding: 20px;
   }
-}
+ }
 </style>
